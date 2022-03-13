fdca043f497338631d447ee226acc7bb62c8d843
Jiahe <yi.jiahe00@gmail.com>
2022-03-13 23:47:37 +0800

    Adjust rates and add secret

--- src/features/game/Units.tsx
+++ src/features/game/Units.tsx
@@ -5,22 +5,30 @@ export interface Unit {
 };
 
 export const units = {
-    "Alpha": {
+    "The": {
         incrementRate: 1,
-        weight: 4,
+        weight: 100000,
     },
-    "Beta": {
-        incrementRate: 2,
-        weight: 2,
+    "Secret": {
+        incrementRate: 5,
+        weight: 10000,
     },
-    "Delta": {
-        incrementRate: 2.5,
-        weight: 1.5,
+    "Word": {
+        incrementRate: 25,
+        weight: 1000,
     },
-    "Gamma": {
-        incrementRate: 3,
+    "Is": {
+        incrementRate: 100,
+        weight: 100,
+    },
+    "...": {
+        incrementRate: 300,
+        weight: 10,
+    },
+    "Nothing": {
+        incrementRate: 500,
         weight: 1,
-    },
+    }
 };
 
 const totalWeight = Object.entries(units).reduce((acc, [_k, v]) => acc + v.weight, 0)
